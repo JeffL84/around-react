2d229664908f126ec9c907b19b28931cf931f994
Jeff Lowenhaupt <jeff.lowenhaupt@gmail.com>
2020-09-12 09:43:07 -0400

    fixed delete

--- src/components/App.js
+++ src/components/App.js
@@ -102,13 +102,13 @@ const [cards, setCards] = React.useState([]);
 function handleCardDelete(card) {
   console.log(cards);
   // check if the card has been deleted? not sure there is an analog to the isliked above...
-  
+
   //send api request 
   api.removeCard(card._id)
   .then((remainingCard) => {
     console.log(remainingCard);
     //create new arrayas done above
-    const remainingCards = cards.map((item) => item._id === card._id ? remainingCard : item);
+    const remainingCards = cards.filter((item) => item._id !== card._id);
     //update the state
       setCards(remainingCards)
   })
